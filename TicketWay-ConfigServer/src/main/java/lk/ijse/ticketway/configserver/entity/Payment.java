2a55a10b56096e842e901c28cb87263741334cad
lakshan <lakshanrashmika16302@gmail.com>
2024-07-04 18:44:54 +0530

    updated

---
+++ TicketWay-ConfigServer/src/main/java/lk/ijse/ticketway/configserver/entity/Payment.java
@@ -0,0 +1,38 @@
+package lk.ijse.ticketway.configserver.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 6/27/2024
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paymentId;
+    private String paymentType;
+    private Double paymentAmount;
+    private String paymentTime;
+    private String paymentStatus;
+    private String paymentLocation;
+    @CreationTimestamp
+    private Timestamp date;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ticket_id", referencedColumnName = "id")
+    private Ticket ticket;
+}
